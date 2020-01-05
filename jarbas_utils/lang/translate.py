8f2c2c3d64d443a14d74743200f7e3d1b53f9681
jarbasal <jarbasai@mailfence.com>
2020-01-05 18:45:47 +0000

    use /tmp/mycroft

--- jarbas_utils/lang/translate.py
+++ jarbas_utils/lang/translate.py
@@ -20,13 +20,13 @@ def translate_text(text, lang="en-us", source_lang=None):
     return tx.text
 
 
-def translate_to_mp3(sentence, lang="en-us", mp3_file="/tmp/chatterbox/tts.mp3"):
+def translate_to_mp3(sentence, lang="en-us", mp3_file="/tmp/mycroft/tts.mp3"):
     if detect_lang(sentence) != lang.split("-")[0]:
         sentence = translate_text(sentence, lang)
     return get_tts(sentence, lang, mp3_file)
 
 
-def say_in_language(sentence, lang="en-us", mp3_file="/tmp/chatterbox/tts.mp3"):
+def say_in_language(sentence, lang="en-us", mp3_file="/tmp/mycroft/tts.mp3"):
     mp3_file = translate_to_mp3(sentence, lang, mp3_file)
     play_mp3(mp3_file)
     return sentence
