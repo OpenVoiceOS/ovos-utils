39e57140b77895dec4293558e4ade688ae90f05f
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2020-12-04 15:44:29 +0000

    get_ip  fix (#1)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- ovos_utils/__init__.py
+++ ovos_utils/__init__.py
@@ -17,6 +17,7 @@
 
 from threading import Thread
 from time import sleep
+import requests
 import os
 from os.path import  isdir, join
 import re
@@ -24,13 +25,21 @@ import socket
 
 
 def get_ip():
-    return [l for l in (
-        [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if
-         not ip.startswith("127.")][:1], [
-            [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s
-             in
-             [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][
-        0][0]
+    # taken from https://stackoverflow.com/a/28950776/13703283
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
+def get_external_ip():
+    return requests.get('https://api.ipify.org').text
 
 
 def get_mycroft_root():
@@ -163,3 +172,4 @@ def camel_case_split(identifier: str) -> str:
 
 def rotate_list(l, n=1):
     return l[n:] + l[:n]
+
