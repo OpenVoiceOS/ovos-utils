553d46c9387de287c79b1f17e5a43d0d748c695d
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2020-12-26 16:50:53 +0000

    better merge dict (#8)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>
    Co-authored-by: ChanceNCounter <ChanceNCounter@icloud.com>

--- ovos_utils/json_helper.py
+++ ovos_utils/json_helper.py
@@ -2,9 +2,11 @@ import json
 from ovos_utils.parse import fuzzy_match
 
 
-def merge_dict(base, delta, merge_lists=False, skip_empty=False, no_dupes=True):
+def merge_dict(base, delta, merge_lists=False, skip_empty=False,
+               no_dupes=True, new_only=False):
     """
-        Recursively merging configuration dictionaries.
+        Recursively merges two dictionaries
+        including dictionaries within dictionaries.
 
         Args:
             base:  Target for merge
@@ -12,21 +14,24 @@ def merge_dict(base, delta, merge_lists=False, skip_empty=False, no_dupes=True):
             merge_lists: if a list is found merge contents instead of replacing
             skip_empty: if an item in delta is empty, dont overwrite base
             no_dupes: when merging lists deduplicate entries
+            new_only: only merge keys not yet in base
     """
 
     for k, d in delta.items():
         b = base.get(k)
         if isinstance(d, dict) and isinstance(b, dict):
-            merge_dict(b, d)
+            merge_dict(b, d, merge_lists, skip_empty, no_dupes, new_only)
         else:
+            if new_only and k in base:
+                continue
             if skip_empty and not d:
                 # dont replace if new entry is empty
                 pass
             elif all((isinstance(b, list), isinstance(d, list), merge_lists)):
-                base[k] += d
                 if no_dupes:
-                    # deduplicate
-                    base[k] = list(set(base[k]))
+                    base[k] += [item for item in d if item not in base[k]]
+                else:
+                    base[k] += d
             else:
                 base[k] = d
     return base
@@ -132,7 +137,8 @@ def get_key_recursively(search_dict, field, filter_None=True):
                     except:
                         continue  # can't parse
                 else:
-                    fields_found += get_key_recursively(item, field, filter_None)
+                    fields_found += get_key_recursively(item,
+                                                        field, filter_None)
 
     return fields_found
 
@@ -158,7 +164,8 @@ def get_key_recursively_fuzzy(search_dict, field, thresh=0.6, filter_None=True):
         if score >= thresh:
             fields_found.append((search_dict, score))
         elif isinstance(value, dict):
-            fields_found += get_key_recursively_fuzzy(value, field, thresh, filter_None)
+            fields_found += get_key_recursively_fuzzy(
+                value, field, thresh, filter_None)
 
         elif isinstance(value, list):
             for item in value:
@@ -169,8 +176,9 @@ def get_key_recursively_fuzzy(search_dict, field, thresh=0.6, filter_None=True):
                     except:
                         continue  # can't parse
                 else:
-                    fields_found += get_key_recursively_fuzzy(item, field, thresh, filter_None)
-    return sorted(fields_found, key = lambda i: i[1],reverse=True)
+                    fields_found += get_key_recursively_fuzzy(
+                        item, field, thresh, filter_None)
+    return sorted(fields_found, key=lambda i: i[1], reverse=True)
 
 
 def get_value_recursively(search_dict, field, target_value):
@@ -200,7 +208,8 @@ def get_value_recursively(search_dict, field, target_value):
                     except:
                         continue  # can't parse
                 else:
-                    fields_found += get_value_recursively(item, field, target_value)
+                    fields_found += get_value_recursively(
+                        item, field, target_value)
 
     return fields_found
 
@@ -226,21 +235,24 @@ def get_value_recursively_fuzzy(search_dict, field, target_value, thresh=0.6):
                     if score >= thresh:
                         fields_found.append((search_dict, score))
         elif isinstance(value, dict):
-            fields_found += get_value_recursively_fuzzy(value, field, target_value, thresh)
+            fields_found += get_value_recursively_fuzzy(
+                value, field, target_value, thresh)
 
         elif isinstance(value, list):
             for item in value:
                 if not isinstance(item, dict):
                     try:
-                        found = get_value_recursively_fuzzy(item.__dict__, field, target_value, thresh)
+                        found = get_value_recursively_fuzzy(
+                            item.__dict__, field, target_value, thresh)
                         if len(found):
                             fields_found.append((item, found[0][1]))
                     except:
                         continue  # can't parse
                 else:
-                    fields_found += get_value_recursively_fuzzy(item, field, target_value, thresh)
+                    fields_found += get_value_recursively_fuzzy(
+                        item, field, target_value, thresh)
 
-    return sorted(fields_found, key = lambda i: i[1],reverse=True)
+    return sorted(fields_found, key=lambda i: i[1], reverse=True)
 
 
 def jsonify_recursively(thing):
---
+++ test/test_json_helpers.py
@@ -0,0 +1,48 @@
+import unittest
+from copy import deepcopy
+from ovos_utils.json_helper import merge_dict
+
+
+class TestJsonHelpers(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.base_dict = {"one": 1, "two": 2, "three": 3,
+                          "four": ["foo", "bar", "baz"], "five": 50}
+        self.delta_dict = {"two": 2, "three": 30,
+                           "four": [4, 5, 6, "foo"], "five": None}
+
+    def test_merge_dict(self):
+        self.assertEqual(
+            merge_dict(deepcopy(self.base_dict), deepcopy(self.delta_dict)),
+            {"one": 1, "two": 2, "three": 30, "four": [
+                4, 5, 6, "foo"], "five": None}
+        )
+
+    def test_merge_dict_lists(self):
+
+        self.assertEqual(
+            merge_dict(deepcopy(self.base_dict), deepcopy(self.delta_dict),
+                       merge_lists=True, no_dupes=False),
+            {"one": 1, "two": 2, "three": 30, "four": [
+                "foo", "bar", "baz", 4, 5, 6, "foo"], "five": None}
+        )
+
+    def test_merge_dict_skip_empty(self):
+
+        self.assertEqual(
+            merge_dict(deepcopy(self.base_dict), deepcopy(self.delta_dict),
+                       merge_lists=True, skip_empty=True, no_dupes=False),
+            {"one": 1, "two": 2, "three": 30, "four": [
+                "foo", "bar", "baz", 4, 5, 6, "foo"], "five": 50}
+        )
+
+    # Difficult to test because order is not currently guaranteed.
+
+    def test_merge_dict_no_dupes(self):
+
+        self.assertEqual(
+            merge_dict(deepcopy(self.base_dict), deepcopy(self.delta_dict), merge_lists=True,
+                       skip_empty=True, no_dupes=True),
+            {"one": 1, "two": 2, "three": 30, "four": [
+                "foo", "bar", "baz", 4, 5, 6], "five": 50}
+        )
