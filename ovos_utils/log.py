be82ff4c2bda94982d0e43a6b64e99826d377cdc
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2020-12-28 19:48:24 +0000

    better logs (#12)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- ovos_utils/log.py
+++ ovos_utils/log.py
@@ -1,5 +1,4 @@
-# Copyright 2017 Mycroft AI Inc.
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,15 +14,8 @@
 import inspect
 import logging
 import sys
-
-
-def _make_log_method(fn):
-    @classmethod
-    def method(cls, *args, **kwargs):
-        cls._log(fn, *args, **kwargs)
-
-    method.__func__.__doc__ = fn.__doc__
-    return method
+from os.path import join
+from logging.handlers import RotatingFileHandler
 
 
 class LOG:
@@ -37,65 +29,97 @@ class LOG:
         >>> LOG('custom_name').debug('Another message')
         13:13:10.462 - custom_name - DEBUG - Another message
     """
-
-    _custom_name = None
-    handler = None
-    level = None
-
-    # Copy actual logging methods from logging.Logger
-    # Usage: LOG.debug(message)
-    debug = _make_log_method(logging.Logger.debug)
-    info = _make_log_method(logging.Logger.info)
-    warning = _make_log_method(logging.Logger.warning)
-    error = _make_log_method(logging.Logger.error)
-    exception = _make_log_method(logging.Logger.exception)
+    base_path = "stdout"
+    fmt = '%(asctime)s.%(msecs)03d - ' \
+          '%(name)s - %(levelname)s - %(message)s'
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(fmt, datefmt)
+    max_bytes = 50000000
+    backup_count = 3
+    name = 'OVOS'
+    level = "DEBUG"
+    _loggers = {}
 
     @classmethod
-    def init(cls):
-        cls.level = "DEBUG"
-        fmt = '%(asctime)s.%(msecs)03d - ' \
-              '%(name)s - %(levelname)s - %(message)s'
-        datefmt = '%H:%M:%S'
-        formatter = logging.Formatter(fmt, datefmt)
-        cls.handler = logging.StreamHandler(sys.stdout)
-        cls.handler.setFormatter(formatter)
-
-        # Enable logging in external modules
-        cls.create_logger('').setLevel(cls.level)
+    def init(cls, config=None):
+        config = config or {}
+        cls.base_path = config.get("path", "stdout")
+        cls.max_bytes = config.get("max_bytes", 50000000)
+        cls.backup_count = config.get("backup_count", 3)
+        cls.level = config.get("level", "DEBUG")
 
     @classmethod
-    def create_logger(cls, name):
+    def create_logger(cls, name, tostdout=False):
+        if name in cls._loggers:
+            return cls._loggers[name]
         logger = logging.getLogger(name)
         logger.propagate = False
-        logger.addHandler(cls.handler)
+        # also log to stdout
+        if tostdout:
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            stdout_handler.setFormatter(cls.formatter)
+            logger.addHandler(stdout_handler)
+        # log to file
+        if cls.base_path != "stdout":
+            path = join(cls.base_path,
+                        cls.name.lower().strip() + ".log")
+            handler = RotatingFileHandler(path, maxBytes=cls.max_bytes,
+                                          backupCount=cls.backup_count)
+            handler.setFormatter(cls.formatter)
+            logger.addHandler(handler)
+        logger.setLevel(cls.level)
+        cls._loggers[name] = logger
         return logger
 
-    def __init__(self, name):
-        LOG._custom_name = name
+    @classmethod
+    def set_level(cls, level):
+        cls.level = level
+        for l in cls._loggers:
+            cls._loggers[l].setLevel(level)
 
     @classmethod
-    def _log(cls, func, *args, **kwargs):
-        if cls._custom_name is not None:
-            name = cls._custom_name
-            cls._custom_name = None
-        else:
-            # Stack:
-            # [0] - _log()
-            # [1] - debug(), info(), warning(), or error()
-            # [2] - caller
-            stack = inspect.stack()
+    def _get_real_logger(cls):
+        name = ""
+        if cls.name is not None:
+            name = cls.name + " - "
 
-            # Record:
-            # [0] - frame object
-            # [1] - filename
-            # [2] - line number
-            # [3] - function
-            # ...
-            record = stack[2]
-            mod = inspect.getmodule(record[0])
-            module_name = mod.__name__ if mod else ''
-            name = module_name + ':' + record[3] + ':' + str(record[2])
-        func(cls.create_logger(name), *args, **kwargs)
+        # Stack:
+        # [0] - _log()
+        # [1] - debug(), info(), warning(), or error()
+        # [2] - caller
+        stack = inspect.stack()
+
+        # Record:
+        # [0] - frame object
+        # [1] - filename
+        # [2] - line number
+        # [3] - function
+        # ...
+        record = stack[2]
+        mod = inspect.getmodule(record[0])
+        module_name = mod.__name__ if mod else ''
+        name += module_name + ':' + record[3] + ':' + str(record[2])
+
+        return cls.create_logger(name)
+
+    @classmethod
+    def info(cls, *args, **kwargs):
+        cls._get_real_logger().info(*args, **kwargs)
+
+    @classmethod
+    def debug(cls, *args, **kwargs):
+        cls._get_real_logger().debug(*args, **kwargs)
+
+    @classmethod
+    def warning(cls, *args, **kwargs):
+        cls._get_real_logger().warning(*args, **kwargs)
+
+    @classmethod
+    def error(cls, *args, **kwargs):
+        cls._get_real_logger().error(*args, **kwargs)
+
+    @classmethod
+    def exception(cls, *args, **kwargs):
+        cls._get_real_logger().exception(*args, **kwargs)
 
 
-LOG.init()
