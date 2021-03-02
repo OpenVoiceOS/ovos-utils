ee5ab737a2ed7340b6dc8bd4cd5d6de11af029e9
jarbasal <jarbasai@mailfence.com>
2021-03-02 11:46:34 +0000

    split

--- ovos_utils/skills/templates/active.py
+++ ovos_utils/skills/templates/active.py
@@ -14,11 +14,3 @@ class ActiveSkill(MycroftSkill):
         self.make_active()
 
 
-class PassiveSkill(ActiveSkill):
-
-    def handle_utterance(self, utterances, lang="en-us"):
-        """ Listen to all utterances passively, eg, take metrics """
-
-    def converse(self, utterances, lang="en-us"):
-        self.handle_utterance(utterances, lang)
-        return False
