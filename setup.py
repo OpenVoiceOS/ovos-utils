79cc462e52589b4993a783ddfded4af849d26c85
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-08-02 07:32:47 -1000

    fix/optional phoneme_guesser (#76)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ovos_utils',
-    version='0.0.12a1',
+    version='0.0.12a2',
     packages=['ovos_utils',
               'ovos_utils.intents',
               'ovos_utils.sound',
