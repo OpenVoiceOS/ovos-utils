07c024b871e7f5df05c546273fffa285f99c0e75
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-08-10 14:11:28 +0100

    add stopwatch (#78)
    * add stopwatch
    
    * add stopwatch
    
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ovos_utils',
-    version='0.0.12a2',
+    version='0.0.12a3',
     packages=['ovos_utils',
               'ovos_utils.intents',
               'ovos_utils.sound',
