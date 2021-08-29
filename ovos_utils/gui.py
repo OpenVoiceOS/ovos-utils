29c5da4599f86eb430ac5496b1d9e1769dd66e74
jarbasal <jarbasai@mailfence.com>
2021-08-29 14:11:27 +0100

    backport https://github.com/MycroftAI/mycroft-core/pull/2862

--- ovos_utils/gui.py
+++ ovos_utils/gui.py
@@ -645,6 +645,28 @@ class GUIInterface:
                                "__from": self.skill_id}))
 
     # Utils / Templates
+
+    # backport - PR https://github.com/MycroftAI/mycroft-core/pull/2862
+    def show_notification(self, content, action=None,
+                          noticetype="transient"):
+        """Display a Notification on homepage in the GUI.
+        Arguments:
+            content (str): Main text content of a notification, Limited
+            to two visual lines.
+            action (str): Callback to any event registered by the skill
+            to perform a certain action when notification is clicked.
+            noticetype (str):
+                transient: 'Default' displays a notification with a timeout.
+                sticky: displays a notification that sticks to the screen.
+        """
+        self.skill.bus.emit(Message("homescreen.notification.set",
+                                    data={
+                                        "sender": self.skill.skill_id,
+                                        "text": content,
+                                        "action": action,
+                                        "type": noticetype
+                                    }))
+
     def show_text(self, text, title=None, override_idle=None,
                   override_animations=False):
         """Display a GUI page for viewing simple text.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ovos_utils',
-    version='0.0.12a5',
+    version='0.0.12a6',
     packages=['ovos_utils',
               'ovos_utils.intents',
               'ovos_utils.sound',
