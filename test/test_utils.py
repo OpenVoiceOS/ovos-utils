07d71b9cb41fbdf253c8c2f7c6ca89305a1eba7c
jarbasai <jarbasai@mailfence.com>
2022-02-09 22:42:29 +0000

    rm tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,19 +1,8 @@
 import unittest
-from ovos_utils import rotate_list, camel_case_split, camelize, \
-    transliterate, titleize, parameterize, ordinalize, get_handler_name
+from ovos_utils import rotate_list, camel_case_split, get_handler_name
 
 
 class TestHelpers(unittest.TestCase):
-
-    def test_inflection_utils(self):
-        # just imports from inflection package
-        # https://inflection.readthedocs.io/en/latest/
-        self.assertEqual(camelize("device_type"), "DeviceType")
-        self.assertEqual(titleize("MyAwesome_Skill"), "My Awesome Skill")
-        self.assertEqual(transliterate('Ærøskøbing'), 'rskbing')
-        self.assertEqual(parameterize("Donald E. Knuth"), 'donald-e-knuth')
-        self.assertEqual(ordinalize(5), '5th')
-        self.assertEqual(ordinalize(2), '2nd')
 
     def test_utils(self):
         def some_function():
