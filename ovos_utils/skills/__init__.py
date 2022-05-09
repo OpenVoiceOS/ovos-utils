621a54a235928076c8cc9bfd45bcd5c5d12e5d17
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-05-09 21:14:26 +0100

    refactor/no_adapt (#42)
    some helper classes providing only syntactic sugar required adapt-parser, these have been refactored and moved into ovos_utils to avoid dragging adapt-parser dependency (except for skills service)
    
    Co-authored-by: jarbasai <jarbasai@mailfence.com>

--- ovos_utils/skills/__init__.py
+++ ovos_utils/skills/__init__.py
@@ -4,6 +4,31 @@ from os.path import join, isdir, isfile
 from os import listdir
 
 
+def get_non_properties(obj):
+    """Get attributes that are not properties from object.
+
+    Will return members of object class along with bases down to MycroftSkill.
+
+    Args:
+        obj: object to scan
+
+    Returns:
+        Set of attributes that are not a property.
+    """
+
+    def check_class(cls):
+        """Find all non-properties in a class."""
+        # Current class
+        d = cls.__dict__
+        np = [k for k in d if not isinstance(d[k], property)]
+        # Recurse through base classes excluding MycroftSkill and object
+        for b in [b for b in cls.__bases__ if b.__name__ not in ("object", "MycroftSkill")]:
+            np += check_class(b)
+        return np
+
+    return set(check_class(obj.__class__))
+
+
 def skills_loaded(bus=None):
     reply = wait_for_reply('mycroft.skills.all_loaded',
                            'mycroft.skills.all_loaded.response',
