40b5d120ce8f9a76398323f556b807e69a7170b0
Aditya Mehra <aix.m@outlook.com>
2022-08-15 22:18:52 +0930

    Add extend about data method to gui utils (#57)
    * Add extend about data method to gui utils
    
    * add bus arg to method
    
    * fix arg position

--- ovos_utils/gui.py
+++ ovos_utils/gui.py
@@ -49,6 +49,15 @@ def can_use_gui(bus=None, local=False):
         return can_use_local_gui()
     return can_use_local_gui() or is_gui_connected(bus)
 
+def extend_about_data(about_data, bus=None):
+    bus = bus or get_mycroft_bus()
+    if isinstance(about_data, list):
+        bus.emit(Message("smartspeaker.extension.extend.about", {"display_list": about_data}))
+    elif isinstance(about_data, dict):
+        display_list = [about_data]
+        bus.emit(Message("smartspeaker.extension.extend.about", {"display_list": display_list}))
+    else:
+        LOG.error("about_data is not a list or dictionary")
 
 class GUIWidgets:
     def __init__(self, bus=None):
