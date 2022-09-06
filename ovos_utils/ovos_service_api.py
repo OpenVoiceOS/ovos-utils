9fecc035180598e0e0868eede5afd5f9e31fde48
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-09-06 09:39:46 +0100

    feat/ovos_api (#60)
    * feat/ovos_api
    
    add helper classes for all ovos api endpoints
    
    * add more end points
    
    Co-authored-by: Aditya Mehra <aix.m@outlook.com>

--- ovos_utils/ovos_service_api.py
+++ ovos_utils/ovos_service_api.py
@@ -1,4 +1,3 @@
-import json
 import requests
 from json_database import JsonStorageXDG
 
@@ -36,3 +35,102 @@ class OVOSApiService:
 
     def get_session_token(self):
         return self.token_storage.get("challenge", "")
+
+
+class OvosWeather:
+    def __init__(self):
+        self.api = OVOSApiService()
+
+    @property
+    def uuid(self):
+        return self.api.get_uuid()
+
+    @property
+    def headers(self):
+        self.api.get_session_challenge()
+        return {'session_challenge': self.api.get_session_token(),  'backend': 'OWM'}
+
+    def get_weather_onecall(self, query):
+        reqdata = {"lat": query.get("lat"),
+                   "lon": query.get("lon"),
+                   "units": query.get("units"),
+                   "lang": query.get("lang")}
+        url = f'https://api.openvoiceos.com/weather/onecall_weather_report/{self.uuid}'
+        r = requests.post(url, data=reqdata, headers=self.headers)
+        return r.json()
+
+class OvosWolframAlpha:
+    def __init__(self):
+        self.api = OVOSApiService()
+
+    @property
+    def uuid(self):
+        return self.api.get_uuid()
+
+    @property
+    def headers(self):
+        self.api.get_session_challenge()
+        return {'session_challenge': self.api.get_session_token()}
+
+    def get_wolfram_spoken(self, query):
+        reqdata = {"input": query.get("input"),
+                   "units": query.get("units")}
+        url = f'https://api.openvoiceos.com/wolframalpha/spoken/{self.uuid}'
+        r = requests.post(url, data=reqdata, headers=self.headers)
+        return r
+    
+    def get_wolfram_simple(self, query):
+        reqdata = {"input": query.get("input"),
+                   "units": query.get("units")}
+        url = f'https://api.openvoiceos.com/wolframalpha/simple/{self.uuid}'
+        r = requests.post(url, data=reqdata, headers=self.headers)
+        return r
+    
+    def get_wolfram_full(self, query):
+        reqdata = {"input": query.get("input"),
+                   "units": query.get("units")}
+        url = f'https://api.openvoiceos.com/wolframalpha/full/{self.uuid}'
+        r = requests.post(url, data=reqdata, headers=self.headers)
+        return r.json()
+
+class OvosEdamamRecipe:
+    def __init__(self):
+        self.api = OVOSApiService()
+        
+    @property
+    def uuid(self):
+        return self.api.get_uuid()
+
+    @property
+    def headers(self):
+        self.api.get_session_challenge()
+        return {'session_challenge': self.api.get_session_token()}
+    
+    def get_recipe(self, query):
+        reqdata = {"query": query.get("query"),
+                   "count": query.get("count", 5)}
+        url = f'https://api.openvoiceos.com/recipes/search_recipe/'
+        r = requests.post(url, data=reqdata, headers=self.headers)
+        return r.json()
+    
+class OvosOmdb:
+    def __init__(self):
+        self.api = OVOSApiService()
+        
+    @property
+    def uuid(self):
+        return self.api.get_uuid()
+    
+    @property
+    def headers(self):
+        self.api.get_session_challenge()
+        return {'session_challenge': self.api.get_session_token()}
+
+    def search_movie(self, query):
+        reqdata = {"movie_name": query.get("movie_name"),
+                   "movie_year": query.get("movie_year"),
+                   "movie_id": query.get("movie_id")}
+        
+        url = f'https://api.openvoiceos.com/omdb/search_movie/'
+        r = requests.post(url, data=reqdata, headers=self.headers)
+        return r.json()
