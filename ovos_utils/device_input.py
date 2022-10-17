e8020b9bcd296219fe4602da4e9e5cdf2b9ab167
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-10-17 19:22:36 +0100

    feat/xinput support (#74)
    * feat/xinput support
    
    add support for `xinput list` for mouse detection
    
    * revert debug logs

--- ovos_utils/device_input.py
+++ ovos_utils/device_input.py
@@ -4,17 +4,18 @@ from distutils.spawn import find_executable
 
 class InputDeviceHelper:
     def __init__(self) -> None:
-        self.input_devices_list = []
-        if not find_executable("libinput"):
-            raise Exception("libinput executable not found")
+        self.libinput_devices_list = []
+        self.xinput_devices_list = []
+        if not find_executable("libinput") and not find_executable("xinput"):
+            raise Exception("xinput/libinput executable not found")
 
     # ToDo: add support for discovring the input device based of a connected
     # monitors, currently linux only supports input listing directly from the
     # system
 
-    def build_input_devices_list(self):
+    def _build_linput_devices_list(self):
         # Always clear the list before building it
-        self.input_devices_list.clear()
+        self.libinput_devices_list.clear()
 
         input_device_names = []
         input_device_kernel_path = []
@@ -22,8 +23,7 @@ class InputDeviceHelper:
         input_device_capabilities = []
 
         # Get the list of devices from libinput
-        proc_output = subprocess.check_output(
-            ['libinput', 'list-devices']).decode('utf-8')
+        proc_output = subprocess.check_output(['libinput', 'list-devices']).decode('utf-8')
         for line in proc_output.splitlines():
             if line.startswith('Device'):
                 unformated_device_name = line.split(':')[1]
@@ -58,33 +58,62 @@ class InputDeviceHelper:
                     unformated_device_capabilities)
 
         for i in range(len(input_device_names)):
-            self.input_devices_list.append(
+            self.libinput_devices_list.append(
                 {"Device": input_device_names[i],
                  "Kernel": input_device_kernel_path[i],
                  "Group": input_device_group[i],
                  "Capabilities": input_device_capabilities[i]
                  })
 
-    def get_input_devices_list(self):
-        self.build_input_devices_list()
-        return self.input_devices_list
+    def _get_libinput_devices_list(self):
+        self._build_linput_devices_list()
+        return self.libinput_devices_list
+
+    def _build_xinput_devices_list(self):
+        # Always clear the list before building it
+        self.xinput_devices_list.clear()
+
+        # Get the list of devices from xinput
+        proc_output = subprocess.check_output(['xinput', 'list']).decode('utf-8')
+        for line in proc_output.splitlines():
+            if "id=" not in line:
+                continue
+            line = line.replace("↳", "").replace("⎡", "").replace("⎣", "").replace("⎜", "").strip()
+
+            if "pointer" in line:
+                dev = {"Device": line.split("id=")[0].strip(),
+                       "Capabilities": ["mouse"]}
+                self.xinput_devices_list.append(dev)
+            if "keyboard" in line:
+                dev = {"Device": line.split("id=")[0].strip(),
+                       "Capabilities": ["keyboard"]}
+                self.xinput_devices_list.append(dev)
+
+    def _get_xinput_devices_list(self):
+        self._build_xinput_devices_list()
+        return self.xinput_devices_list
+
+    def get_input_device_list(self):
+        # check if any of the devices support touch or mouse
+        if find_executable("libinput"):
+            self._build_linput_devices_list()
+        if find_executable("xinput"):
+            self._build_xinput_devices_list()
+        return self.libinput_devices_list + self.xinput_devices_list
 
     def can_use_touch_mouse(self):
-        # check if any of the devices support touch or mouse
-        self.build_input_devices_list()
-        for device in self.input_devices_list:
-            if "touch" in device["Capabilities"] or "mouse" in device[
-                    "Capabilities"] or "tablet" in device["Capabilities"] or "gesture" in device["Capabilities"]:
+        for device in self.get_input_device_list():
+            if "touch" in device["Capabilities"] or \
+                    "mouse" in device["Capabilities"] or \
+                    "tablet" in device["Capabilities"] or \
+                    "gesture" in device["Capabilities"]:
                 return True
-
         return False
 
     def can_use_keyboard(self):
-        self.build_input_devices_list()
-        for device in self.input_devices_list:
+        for device in self.get_input_device_list():
             if "keyboard" in device["Capabilities"]:
                 return True
-
         return False
 
 
