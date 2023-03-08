a04f971a84299ceefb8f22fb58f8f8e6b71b1025
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-03-08 14:50:34 +0000

    feat/console_scripts (#105)
    * feat/console_scripts
    
    each method here is a entrypoint define in setup.py, each corresponds to a cli util
    
    ```
        entry_points={
            'console_scripts': [
                'ovos-listen=ovos_utils.scripts:ovos_listen',
                'ovos-speak=ovos_utils.scripts:ovos_speak',
                'ovos-say_to=ovos_utils.scripts:ovos_say_to',
            ]
        }
    
    ```
    
    * typo

---
+++ ovos_utils/scripts.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# each method here is a console_script defined in setup.py
+# each corresponds to a cli util
+from mycroft_bus_client import MessageBusClient, Message
+from ovos_config import Configuration
+import sys
+
+
+def ovos_speak():
+    if (args_count := len(sys.argv)) == 2:
+        utt = sys.argv[1]
+        lang = Configuration().get("lang", "en-us")
+    elif args_count == 3:
+        utt = sys.argv[1]
+        lang = sys.argv[2]
+    else:
+        print("USAGE: ovos-speak {utterance} [lang]")
+        raise SystemExit(2)
+    client = MessageBusClient()
+    client.run_in_thread()
+    client.emit(Message("speak", {"utterance": utt, "lang": lang}))
+    client.close()
+
+
+def ovos_say_to():
+    if (args_count := len(sys.argv)) == 2:
+        utt = sys.argv[1]
+        lang = Configuration().get("lang", "en-us")
+    elif args_count == 3:
+        utt = sys.argv[1]
+        lang = sys.argv[2]
+    else:
+        print("USAGE: ovos-say-to {utterance} [lang]")
+        raise SystemExit(2)
+    client = MessageBusClient()
+    client.run_in_thread()
+    client.emit(Message("recognizer_loop:utterance", {"utterances": [utt], "lang": lang}))
+    client.close()
+
+
+def ovos_listen():
+    client = MessageBusClient()
+    client.run_in_thread()
+    client.emit(Message("mycroft.mic.listen"))
+    client.close()
