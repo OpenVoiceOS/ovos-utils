41aac79e7560a38af1b6d55e643c373b1733c62c
JarbasAi <jarbasai@mailfence.com>
2023-04-11 00:41:11 +0100

    unitest fix

--- test/unittests/test_skills.py
+++ test/unittests/test_skills.py
@@ -6,6 +6,10 @@ from ovos_utils.skills.locations import get_skill_directories
 from ovos_utils.skills.locations import get_default_skills_directory
 from ovos_utils.skills.locations import get_installed_skill_ids
 from ovos_utils.skills.locations import get_plugin_skills
+try:
+    import ovos_config
+except ImportError:
+    ovos_config = None
 
 
 class TestLocations(unittest.TestCase):
@@ -23,6 +27,9 @@ class TestLocations(unittest.TestCase):
                                           "skill-test-2.openvoiceos"})
 
     def test_get_skill_directories(self):
+        if not ovos_config:
+            return  # skip test since ovos.conf isn't taken into account
+
         # Default behavior, only one valid XDG path
         environ["XDG_DATA_DIRS"] = environ["XDG_DATA_HOME"] = \
             join(dirname(__file__), "test_skills_xdg")
@@ -42,12 +49,15 @@ class TestLocations(unittest.TestCase):
         self.assertEqual(get_skill_directories(config),
                          [default_dir, extra_dir])
 
+
         # Define invalid directories in extra_directories
         config['skills']['extra_directories'] += ["/not/a/directory"]
         self.assertEqual(get_skill_directories(config),
                          [default_dir, extra_dir])
 
     def test_get_default_skills_directory(self):
+        if not ovos_config:
+            return  # skip test since ovos.conf isn't taken into account
         test_skills_dir = join(dirname(__file__), "test_skills_dir")
 
         # Configured override (legacy)
