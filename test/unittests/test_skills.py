71a7d59bcd96326a38a7289c01033af31d4d7b0a
JarbasAi <jarbasai@mailfence.com>
2023-04-11 00:31:27 +0100

    imports

--- test/unittests/test_skills.py
+++ test/unittests/test_skills.py
@@ -2,6 +2,10 @@ import unittest
 from os import environ
 from os.path import isdir, join, dirname
 from unittest.mock import patch
+from ovos_utils.skills.locations import get_skill_directories
+from ovos_utils.skills.locations import get_default_skills_directory
+from ovos_utils.skills.locations import get_installed_skill_ids
+from ovos_utils.skills.locations import get_plugin_skills
 
 
 class TestLocations(unittest.TestCase):
@@ -9,7 +13,6 @@ class TestLocations(unittest.TestCase):
     def test_get_installed_skill_ids(self, plugins):
         plugins.return_value = (['plugin_dir', 'plugin_dir_2'],
                                 ['plugin_id', 'plugin_id_2'])
-        from ovos_utils.skills.locations import get_installed_skill_ids
         environ["XDG_DATA_DIRS"] = join(dirname(__file__), "test_skills_xdg")
         config = {"skills": {
             "extra_directories": [join(dirname(__file__), "test_skills_dir")]
@@ -20,8 +23,6 @@ class TestLocations(unittest.TestCase):
                                           "skill-test-2.openvoiceos"})
 
     def test_get_skill_directories(self):
-        from ovos_utils.skills.locations import get_skill_directories
-
         # Default behavior, only one valid XDG path
         environ["XDG_DATA_DIRS"] = environ["XDG_DATA_HOME"] = \
             join(dirname(__file__), "test_skills_xdg")
@@ -47,7 +48,6 @@ class TestLocations(unittest.TestCase):
                          [default_dir, extra_dir])
 
     def test_get_default_skills_directory(self):
-        from ovos_utils.skills.locations import get_default_skills_directory
         test_skills_dir = join(dirname(__file__), "test_skills_dir")
 
         # Configured override (legacy)
@@ -70,7 +70,6 @@ class TestLocations(unittest.TestCase):
         self.assertEqual(get_default_skills_directory(config), xdg_skills_dir)
 
     def test_get_plugin_skills(self):
-        from ovos_utils.skills.locations import get_plugin_skills
         dirs, ids = get_plugin_skills()
         for d in dirs:
             self.assertTrue(isdir(d))
