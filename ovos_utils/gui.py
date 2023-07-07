6b9c13fb3c8288ebae5905e95355d44e4308a789
Daniel McKnight <daniel@neongecko.com>
2023-07-07 14:16:05 -0700

    Announce when GUI files are available on GUIInterface init Replaces https://github.com/OpenVoiceOS/OVOS-workshop/pull/109

--- ovos_utils/gui.py
+++ ovos_utils/gui.py
@@ -556,6 +556,11 @@ class GUIInterface:
         if bus:
             self.set_bus(bus)
 
+        if self.ui_directories:
+            self.bus.emit(Message("gui.volunteer_page_upload",
+                                  {'skill_id': skill_id},
+                                  {'source': skill_id, "destination": ["gui"]}))
+
     @property
     def remote_url(self) -> Optional[str]:
         """Returns configuration value for url of remote-server."""
