360c0776afab2bfd5d17d99fd2b36afc2d9a380a
Daniel McKnight <daniel@neongecko.com>
2023-08-03 13:59:47 -0700

    Improved error logging in API timeouts

--- ovos_utils/skills/api.py
+++ ovos_utils/skills/api.py
@@ -51,7 +51,8 @@ class SkillApi:
                         SkillApi.bus.wait_for_response(method_msg,
                                                        timeout=self.timeout)
                     if not response:
-                        LOG.error(f"Timed out waiting for {method_msg}")
+                        LOG.error(f"Timed out waiting for response to: "
+                                  f"{method_msg.msg_type} ({self.timeout}")
                         return None
                     elif 'result' not in response.data:
                         LOG.error(f"missing `result` in: {response.data}")
