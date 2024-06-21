a05ab7bce24d4ff68c7943650e9d41274ec6d5fa
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-06-21 18:20:37 +0100

    feat/ocp_stream_utils (#257)
    * feat/ocp_stream_utils
    
    helper methods to parse the ocp stream from PluginStream objects
    
    * more tests
    
    * more tests

--- test/unittests/test_ocp_media.py
+++ test/unittests/test_ocp_media.py
@@ -2,7 +2,7 @@ import unittest
 
 from ovos_utils.ocp import MediaEntry, Playlist, MediaType, PlaybackType, TrackState
 
-valid_search_results = [
+dict_search_results = [
     {'media_type': MediaType.MUSIC,
      'playback': PlaybackType.AUDIO,
      'image': 'https://freemusicarchive.org/legacy/fma-smaller.jpg',
@@ -17,6 +17,7 @@ valid_search_results = [
      'image': 'https://freemusicarchive.org/legacy/fma-smaller.jpg',
      'skill_icon': 'https://freemusicarchive.org/legacy/fma-smaller.jpg',
      'uri': 'https://freemusicarchive.org/track/05_-_Quantum_Jazz_-_Passing_Fields/stream/',
+     'skill_id': 'skill-free_music_archive.neongeckocom',
      'title': 'Passing Fields',
      'artist': 'Quantum Jazz',
      'match_confidence': 65},
@@ -24,16 +25,48 @@ valid_search_results = [
      'playback': PlaybackType.AUDIO,
      'image': 'https://freemusicarchive.org/legacy/fma-smaller.jpg',
      'skill_icon': 'https://freemusicarchive.org/legacy/fma-smaller.jpg',
+     'skill_id': 'skill-free_music_archive.neongeckocom',
      'uri': 'https://freemusicarchive.org/track/04_-_Quantum_Jazz_-_All_About_The_Sun/stream/',
      'title': 'All About The Sun',
      'artist': 'Quantum Jazz',
      'match_confidence': 65}
 ]
 
+search_results = [
+    MediaEntry(media_type=MediaType.MUSIC,
+               playback=PlaybackType.AUDIO,
+               image='https://freemusicarchive.org/legacy/fma-smaller.jpg',
+               uri='https://freemusicarchive.org/track/07_-_Quantum_Jazz_-_Orbiting_A_Distant_Planet/stream/',
+               skill_icon='https://freemusicarchive.org/legacy/fma-smaller.jpg',
+               title='Orbiting A Distant Planet',
+               artist='Quantum Jazz',
+               skill_id='skill-free_music_archive.neongeckocom',
+               match_confidence=65
+               ),
+    MediaEntry(media_type=MediaType.MUSIC,
+               playback=PlaybackType.AUDIO,
+               image='https://freemusicarchive.org/legacy/fma-smaller.jpg',
+               uri='https://freemusicarchive.org/track/05_-_Quantum_Jazz_-_Passing_Fields/stream/',
+               skill_icon='https://freemusicarchive.org/legacy/fma-smaller.jpg',
+               title='Passing Fields',
+               artist='Quantum Jazz',
+               skill_id='skill-free_music_archive.neongeckocom',
+               match_confidence=65),
+    MediaEntry(media_type=MediaType.MUSIC,
+               playback=PlaybackType.AUDIO,
+               image='https://freemusicarchive.org/legacy/fma-smaller.jpg',
+               uri='https://freemusicarchive.org/track/04_-_Quantum_Jazz_-_All_About_The_Sun/stream/',
+               skill_icon='https://freemusicarchive.org/legacy/fma-smaller.jpg',
+               title='All About The Sun',
+               artist='Quantum Jazz',
+               skill_id='skill-free_music_archive.neongeckocom',
+               match_confidence=65)
+]
+
 
 class TestMediaEntry(unittest.TestCase):
     def test_init(self):
-        data = valid_search_results[0]
+        data = dict_search_results[0]
 
         # Test MediaEntry init
         entry = MediaEntry(**data)
@@ -52,8 +85,17 @@ class TestMediaEntry(unittest.TestCase):
         new_entry = MediaEntry(**data)
         self.assertEqual(entry, new_entry)
 
+    def test_as_dict(self):
+        for idx, track in enumerate(search_results):
+            for k, v in track.as_dict.items():
+                if k in dict_search_results[idx]:
+                    self.assertEqual(v, dict_search_results[idx][k])
+
+        for idx, track in enumerate(dict_search_results):
+            self.assertEqual(MediaEntry.from_dict(track), search_results[idx])
+
     def test_from_dict(self):
-        dict_data = valid_search_results[1]
+        dict_data = dict_search_results[1]
         from_dict = MediaEntry.from_dict(dict_data)
         self.assertIsInstance(from_dict, MediaEntry)
         from_init = MediaEntry(dict_data["uri"], dict_data["title"],
@@ -61,6 +103,7 @@ class TestMediaEntry(unittest.TestCase):
                                match_confidence=dict_data["match_confidence"],
                                playback=PlaybackType.AUDIO,
                                skill_icon=dict_data["skill_icon"],
+                               skill_id=dict_data["skill_id"],
                                media_type=dict_data["media_type"],
                                artist=dict_data["artist"])
         self.assertEqual(from_init, from_dict)
@@ -85,16 +128,32 @@ class TestPlaylist(unittest.TestCase):
         self.assertTrue(pl.is_last_track)
 
         # Playlist of dicts
-        pl = Playlist(valid_search_results, title="my playlist")
+        pl = Playlist(dict_search_results, title="my playlist")
         self.assertEqual(pl.title, "my playlist")
         self.assertEqual(pl.position, 0)
-        self.assertEqual(len(pl), len(valid_search_results))
-        self.assertEqual(len(pl.entries), len(valid_search_results))
+        self.assertEqual(len(pl), len(dict_search_results))
+        self.assertEqual(len(pl.entries), len(dict_search_results))
         for entry in pl.entries:
             self.assertIsInstance(entry, MediaEntry)
         self.assertIsInstance(pl.current_track, MediaEntry)
         self.assertTrue(pl.is_first_track)
         self.assertFalse(pl.is_last_track)
+
+        # Playlist of MediaEntry
+        pl = Playlist(search_results, title="Test Jazz")
+        self.assertEqual(pl.title, "Test Jazz")
+        self.assertEqual(pl.position, 0)
+        self.assertEqual(len(pl), len(search_results))
+        self.assertEqual(len(pl.entries), len(search_results))
+        for entry in pl.entries:
+            self.assertIsInstance(entry, MediaEntry)
+        self.assertIsInstance(pl.current_track, MediaEntry)
+        self.assertTrue(pl.is_first_track)
+        self.assertFalse(pl.is_last_track)
+
+        self.assertListEqual(pl.entries, search_results)
+        for idx, e in enumerate(pl.as_dict["playlist"]):
+            self.assertEqual(MediaEntry.from_dict(e), search_results[idx])
 
     def test_goto_start(self):
         # TODO
@@ -150,7 +209,7 @@ class TestPlaylist(unittest.TestCase):
         self.assertEqual(pl.position, 0)
 
         # Test playlist of len 1
-        pl = Playlist([valid_search_results[0]])
+        pl = Playlist([dict_search_results[0]])
         pl.position = 0
         pl._validate_position()
         self.assertEqual(pl.position, 0)
@@ -159,7 +218,7 @@ class TestPlaylist(unittest.TestCase):
         self.assertEqual(pl.position, 0)
 
         # Test playlist of len>1
-        pl = Playlist(valid_search_results)
+        pl = Playlist(dict_search_results)
         pl.position = 0
         pl._validate_position()
         self.assertEqual(pl.position, 0)
