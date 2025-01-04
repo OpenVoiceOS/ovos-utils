eb13223ea2ff0d3b464712f0200755d08cb11e9c
miro <jarbasai@mailfence.com>
2025-01-04 17:03:36 +0000

    chore: add warnings
    make IDEs signal deprecated code instead of relying on runtime logs only

--- ovos_utils/messagebus.py
+++ ovos_utils/messagebus.py
@@ -4,3 +4,11 @@ from ovos_utils.log import log_deprecation
 
 log_deprecation("ovos_utils.messagebus has been deprecated since version 0.1.0!! "
                 "please import from ovos_utils.fakebus or ovos_bus_client directly", "1.0.0")
+
+import warnings
+
+warnings.warn(
+    "ovos_utils.messagebus has been deprecated since version 0.1.0",
+    DeprecationWarning,
+    stacklevel=2,
+)
